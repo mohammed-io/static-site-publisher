0f3f88547d0669b033e3d690bf0d91d9b5a11ad5
Mohammed Abdulkereem <mohammed-io@outlook.com>
2020-01-10 21:41:49 +0300

    Added dubai fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      sans:
+        "'Dubai', 'Karla', 'Segoe UI', Roboto, 'Helvetica Neue', sans-serif",
+      serif:
+        "'Dubai', 'Merriweather', 'Georgia', 'Cambria', 'Times New Roman', 'serif'",
+    },
   },
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
