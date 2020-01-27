846a5f2174f96c30936f40aef74176860b4dc8d2
Mohammed Abdulkereem <mohammed-io@outlook.com>
2020-01-27 20:15:37 +0300

    Used json() helper

--- src/routes/blog/[slug].json.js
+++ src/routes/blog/[slug].json.js
@@ -1,33 +1,21 @@
 import { getPosts, findPostBySlug } from '../../blog/posts-repository';
 
 export async function get(req, res, next) {
-  // the `slug` parameter is available because
-  // this file is called [slug].json.js
   const { slug } = req.params;
 
   return (await findPostBySlug(slug))
     .map(post => {
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
+      return res.json({
+        success: true,
+        data: post,
       });
-
-      return res.end(
-        JSON.stringify({
-          success: true,
-          data: post,
-        })
-      );
     })
     .getOr(() => {
-      res.writeHead(404, {
-        'Content-Type': 'application/json',
-      });
+      res.statusCode = 404;
 
-      res.end(
-        JSON.stringify({
-          success: false,
-          message: `Not found`,
-        })
-      );
+      res.json({
+        success: false,
+        message: `Not found`,
+      });
     });
 }
