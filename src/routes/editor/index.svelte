dc65bb47a8134b25b7e2137983de98bdec030506
Mohammed Abdulkereem <mohammed-io@outlook.com>
2020-01-28 14:38:31 +0300

    Added button for new post

--- src/routes/editor/index.svelte
+++ src/routes/editor/index.svelte
@@ -11,15 +11,24 @@
   });
 </script>
 
+<div class="py-3 my-5">
+  <a
+    href="/editor/new"
+    class="btn text-blue-500 hover:text-blue-600 border border-blue-500
+    hover:border-blue-600">
+    New Post
+  </a>
+</div>
 {#each allPosts as post (post.slug)}
   <a href="/editor/{post.slug}">
-    <article class="p-3 my-3 rounded shadow">
+    <article class="p-3 my-5 rounded shadow">
       <time class="text-sm text-gray-700">
         {moment(post.updatedAt).format('MMM DD, YYYY')}
       </time>
       <h1 class="font-bold text-3xl py-4">{post.title}</h1>
       <p>
-        {@html post.body.substring(0, 200)}...
+        {@html post.body.substring(0, 200)}
+        ...
       </p>
     </article>
   </a>
