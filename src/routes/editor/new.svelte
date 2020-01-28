7a7fbe08ef347cf4418105c896749134cccac027
Mohammed Abdulkereem <mohammed-io@outlook.com>
2020-01-28 14:30:52 +0300

    The edit/add post works

--- src/routes/editor/new.svelte
+++ src/routes/editor/new.svelte
@@ -1,5 +1,27 @@
 <script>
   import Loadable from 'svelte-loadable/Loadable.svelte';
+
+  export let post = {};
+
+  async function save() {
+    await fetch(`/editor.json`, {
+      body: JSON.stringify(post),
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(res => res.json())
+      .then(console.log);
+  }
 </script>
 
-<Loadable loader={() => import('../../components/editor/PostEditor.svelte')} />
+<Loadable
+  loader={() => import('../../components/editor/PostEditor.svelte')}
+  let:component>
+  <svelte:component
+    this={component}
+    bind:body={post.body}
+    bind:slug={post.slug}
+    bind:title={post.title}
+    bind:meta={post.meta}
+    on:save={save} />
+</Loadable>
