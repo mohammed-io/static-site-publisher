a6e8931e42c7a8697e3a1bc6f88e9d28b17deadf
Mohammed Abdulkereem <mohammed-io@outlook.com>
2020-02-08 01:49:56 +0300

    Added primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+      },
+    },
     fontFamily: {
       sans: "'Cairo', 'Segoe UI', Roboto, 'Helvetica Neue', sans-serif",
       serif:
